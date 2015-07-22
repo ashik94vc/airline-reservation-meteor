2605fce5d7ed50742c7fc35026ceef444f3d603f
Ashik Vetrivelu <vcashik@gmail.com>
2015-07-22 14:14:13 +0530

    Added ejs view engine

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ function connectionCallback(error,db)
 }
 
 app.get('/', function (req, res) {
-  res.render('hello.html',{title: "HelloWorld"})
+  res.render('hello',{title: "HelloWorld"})
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(5000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
