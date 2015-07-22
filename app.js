cf0a01f9bef255268e2ca6fbbbb90c61372127c4
Ashik Vetrivelu <vcashik@gmail.com>
2015-07-22 14:00:58 +0530

    Working Databinding

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 var express = require('express')
+var ejs = require('ejs')
 var http = require('http')
 var app = express()
+var mongo = require('mongodb').MongoClient;
 
-
+app.set('view engine','html')
+app.engine('html',ejs.renderFile)
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/img',   express.static(__dirname + '/public/img'));
+app.set('views',__dirname+'/public')
+mongo.connect("mongodb://localhost:27017/airline_db",connectionCallback);
+var place = ''
+function connectionCallback(error,db)
+{
+  if(error)
+    return console.error(error)
+  var collection = db.collection('flight_details')
+  collection.find().toArray(function(error,items)
+  {
+    place = console.log(items[1].Source)
+  })
+}
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html')
+  res.render('hello.html',{title: "HelloWorld"})
 });
 
 var server = app.listen(3000, function () {
