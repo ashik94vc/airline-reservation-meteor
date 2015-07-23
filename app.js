3a10f1cd6418c35c30e1f56f39f875578175b55e
Ashik Vetrivelu <vcashik@gmail.com>
2015-07-23 07:20:33 +0530

    Working DB

--- app.js
+++ app.js
@@ -2,31 +2,43 @@ var express = require('express')
 var ejs = require('ejs')
 var http = require('http')
 var app = express()
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose')
 
+mongoose.connect("mongodb://localhost:27017/airline_db",function(error){
+  if(error)
+    console.error(error)
+});
+
+var flightSchema = mongoose.Schema({
+})
+var flights = mongoose.model('flight_details',flightSchema)
 app.set('view engine','html')
 app.engine('html',ejs.renderFile)
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/img',   express.static(__dirname + '/public/img'));
 app.set('views',__dirname+'/public')
-mongo.connect("mongodb://localhost:27017/airline_db",connectionCallback);
-var place = ''
+
 function connectionCallback(error,db)
 {
   if(error)
     return console.error(error)
   var collection = db.collection('flight_details')
-  collection.find().toArray(function(error,items)
-  {
-    place = console.log(items[1].Source)
-  })
+  return collection
 }
-
+flights.find({},function (error,data)
+{
+  console.log(data[0]['Source'])
+})
 app.get('/', function (req, res) {
-  res.render('hello',{title: "HelloWorld"})
+  res.render('hello')
 });
-
+app.get('/schedule',function(req,res) {
+  flights.find({},function (error,data)
+  {
+    res.json(data)
+  })
+})
 var server = app.listen(5000, function () {
   var host = server.address().address;
   var port = server.address().port;
